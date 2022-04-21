04f6509334d92cfe3ba20522f61331d9e6f0847a
Julio Henrique Oliveira Prudente <66337028+juliohenriqueop@users.noreply.github.com>
2022-04-21 16:58:47 -0300

    backend knexfile.js

--- knowledge/backend/src/configuration/knexfile.js
+++ knowledge/backend/src/configuration/knexfile.js
@@ -1,11 +1,15 @@
 module.exports = {
   debug: process.env.NODE_ENV === 'development',
   client: process.env.SQL_DATABASE_CLIENT,
-  connection: {
+  connection: process.env.DATABASE_URL ? {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  } : {
     host: process.env.SQL_DATABASE_HOST,
     database: process.env.SQL_DATABASE_NAME,
     user: process.env.SQL_DATABASE_USER,
-    password: process.env.SQL_DATABASE_PASSWORD
+    password: process.env.SQL_DATABASE_PASSWORD,
+    ssl: { rejectUnauthorized: false },
   },
   migrations: {
     tableName: process.env.SQL_DATABASE_TABLE_MIGRATIONS,
