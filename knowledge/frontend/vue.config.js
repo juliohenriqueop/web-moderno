9dc9e2f5797516b6f8351b1a62459cb5f5e8c39d
Julio Henrique Oliveira Prudente <66337028+juliohenriqueop@users.noreply.github.com>
2022-04-22 01:55:24 -0300

    frontend vue.runtime.min.js

--- knowledge/frontend/vue.config.js
+++ knowledge/frontend/vue.config.js
@@ -2,9 +2,16 @@ const path = require('path')
 
 module.exports = {
   chainWebpack: config => {
-    config.resolve.alias.set(
-      'vue$',
-      path.resolve(__dirname, 'node_modules/vue/dist/vue.js')
-    )
+    if (process.env.NODE_ENV === 'production') {
+      config.resolve.alias.set(
+        'vue$',
+        path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.min.js')
+      )
+    } else {
+      config.resolve.alias.set(
+        'vue$',
+        path.resolve(__dirname, 'node_modules/vue/dist/vue.js')
+      )
+    }
   }
 }
